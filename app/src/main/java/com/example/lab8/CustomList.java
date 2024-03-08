f0016db07a545d4c49af5fc033ce973bf56ab749
Alex Xie <zxie8@ualberta.ca>
2024-03-08 05:37:27 -0700

    Add fail test

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -45,12 +45,31 @@ public class CustomList extends ArrayAdapter<City> {
 
     }
 
+
+
+    /**
+     * this gets size of the list
+     * @return
+     */
     public int getCount(){
         return cities.size();
     }
-
+    /**
+     * this adds a city object to the list
+     *for the first phase it will be
+     empty * @param city
+     */
     public void addCity(City city){
-
     }
+    private CustomList list;
+    /**
+     * create a mocklist for my citylist
+     * @return
+     */
+    public CustomList MockCityList(){
+        list = new CustomList(null,new ArrayList<>());
+        return list;
+    }
+
 
 }
